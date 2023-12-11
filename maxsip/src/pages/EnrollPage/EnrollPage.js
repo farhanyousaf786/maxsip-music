987ae17611ea77e6fb7dd81f9c27597dab1ef577
Farhan Yousaf <58497319+farhanyousaf786@users.noreply.github.com>
2023-12-11 13:36:39 -0500

    4

--- maxsip/src/pages/EnrollPage/EnrollPage.js
+++ maxsip/src/pages/EnrollPage/EnrollPage.js
@@ -12,19 +12,26 @@ const LandingPage = () => {
     try {
       console.log(email);
       console.log(username);
-
-      const response = await axios.get(
-        "/authenticate",
-        {
-          params: {
-            email: email,
-            user: username,
+      const config = {
+        url: "/authenticate",
+        method: "POST",
+        data: {
+          email: email,
+          user: username,
+          header: {
+            Connection: "keep-alive",
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            "Accept-Encoding": "gzip,deflate,br",
+            "Accept-Language": "en-US,en:q=0.8",
+            "x-api-key": "8756e991-685e-4990-a62c-36e3ce0ae983",
           },
-        }
-      );
+        },
+      
+      }
 
+      const response = await axios(config)
       console.log(response.data.url);
-
       if (response.data.url) {
         window.open(response.data.url, "_blank"); // Open received URL in a new tab
       } else {
@@ -38,8 +45,7 @@ const LandingPage = () => {
 
   return (
     <div className="landing-page-container">
-
-      <div className="song-text" >Song redemption</div>
+      <div className="song-text" >Song Redemption</div>
       <div className="input-container">
         <label>Email: </label>
         <input
